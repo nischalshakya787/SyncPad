735549cbd5d07a5a54cb157799fd9006b9353fcc
nischalshakya <107358076+nischalshakya787@users.noreply.github.com>
2024-10-10 14:26:21 +0545

    pages setup

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,14 +1,15 @@
 import "./App.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Home, Auth, Document } from "./pages";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={} />
-        <Route path="/login" element={} />
-        <Route path="/sign-up" element={} />
-        <Route path="/document" element={} />
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Auth />} />
+        <Route path="/sign-up" element={<Auth />} />
+        <Route path="/document" element={<Document />} />
       </Routes>
     </BrowserRouter>
   );

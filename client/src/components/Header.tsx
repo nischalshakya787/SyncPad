f251e40955af2e97d155aa6778a8e73de96bd5d9
nischalshakya <107358076+nischalshakya787@users.noreply.github.com>
2024-10-11 14:11:36 +0545

    Header

---
+++ client/src/components/Header.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Header: React.FC = () => {
+  return (
+    <header className="flex flex-wrap gap-5 justify-between px-8 py-3.5 w-full bg-gray-100 shadow-sm max-md:px-5 max-md:max-w-full">
+      <div className="px-2 text-xs text-black whitespace-nowrap rounded-full bg-zinc-300 h-[50px] w-[50px] flex items-center justify-center">
+        LOGO
+      </div>
+      <button className="px-6 py-4 text-base font-bold text-white bg-blue-500 rounded-lg max-md:px-5">
+        Sign in
+      </button>
+    </header>
+  );
+};
+
+export default Header;

bd04308dec0daacefcf0b9aa0f83eee3f611c392
nischalshakya <107358076+nischalshakya787@users.noreply.github.com>
2024-10-21 19:53:57 +0545

    Document page

--- client/src/pages/Document.tsx
+++ client/src/pages/Document.tsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import ReactQuill from "react-quill";
 import "react-quill/dist/quill.snow.css";
+import { Button } from "../components";
 
 const Document = () => {
   const [value, setValue] = useState("");
@@ -33,14 +34,55 @@ const Document = () => {
     "image",
   ];
   return (
-    <div className="text-editor">
-      <ReactQuill
-        theme="snow"
-        value={value}
-        onChange={setValue}
-        modules={modules}
-        formats={formats}
-      />
+    <div className="text-editor border  h-screen">
+      <div className="flex border border-gray-300 p-2">
+        <div className="w-[75%] flex">
+          <div className="flex items-center justify-center">
+            <div className="w-[35px] h-[35px] bg-red-600 rounded-[100%]">
+              Logo
+            </div>
+          </div>
+          <div className="block ml-3">
+            <div className="text-lg px-3">Untitled Document</div>
+            <div className="menu flex">
+              <div className="buttons px-3 cursor-pointer hover:bg-gray-100">
+                File
+              </div>
+              <div className="buttons px-3 cursor-pointer hover:bg-gray-100">
+                Edit
+              </div>
+              <div className="buttons px-3 cursor-pointer hover:bg-gray-100">
+                View
+              </div>
+              <div className="buttons px-3 cursor-pointer hover:bg-gray-100">
+                Insert
+              </div>
+              <div className="buttons px-3 cursor-pointer hover:bg-gray-100">
+                Format
+              </div>
+              <div className="buttons px-3 cursor-pointer hover:bg-gray-100">
+                Tools
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="w-[25%] flex items-center ">
+          <button className="bg-blue-500 text-white rounded-md p-2">
+            Add Collab
+          </button>
+          <div className="ml-5">Hello, Username</div>
+        </div>
+      </div>
+      <div className="editor h-full flex align-center justify-center py-3">
+        <ReactQuill
+          theme="snow"
+          value={value}
+          onChange={setValue}
+          modules={modules}
+          formats={formats}
+          style={{ width: "900px" }}
+        />
+      </div>
     </div>
   );
 };

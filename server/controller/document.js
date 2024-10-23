17bf92468b05dbf8667a36b03a2f8d652ad3ed79
nischalshakya <107358076+nischalshakya787@users.noreply.github.com>
2024-10-23 19:43:04 +0545

    document create

---
+++ server/controller/document.js
@@ -0,0 +1,19 @@
+import DocumentModel from "../model/Document";
+import UserModel from "../model/User";
+const saveDocument = async (req, res) => {
+  const { title, username } = req.body;
+
+  try {
+    const user = await UserModel.findOne(username);
+
+    if (user) {
+      await DocumentModel.create({
+        title,
+        creator: user,
+      });
+      res.status(200).json({ message: "Document created" });
+    } else {
+      res.status(201).json({ message: "User not found. PLease login" });
+    }
+  } catch (error) {}
+};

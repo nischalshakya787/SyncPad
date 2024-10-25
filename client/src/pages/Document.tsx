a24a05a35e82e72587d3a49090a7a6bd030bb371
nischalshakya <107358076+nischalshakya787@users.noreply.github.com>
2024-10-25 14:23:04 +0545

    save document

--- client/src/pages/Document.tsx
+++ client/src/pages/Document.tsx
@@ -5,17 +5,13 @@ import { formats, modules } from "../constants";
 import { socket } from "../socket";
 import { Delta } from "quill";
 import { UserContext } from "../UserContext";
-
-type userProps = {
-  iat: number;
-  id: string;
-  username: string;
-};
+import { useParams } from "react-router-dom";
 
 const Document = () => {
   const [value, setValue] = useState<string>("");
   const [isTyping, setIsTyping] = useState<boolean>(false);
   const quillRef = useRef<ReactQuill>(null); // Ref for ReactQuill
+  const { id: docId } = useParams<{ id: string }>();
 
   const context = useContext(UserContext);
   if (!context) {
@@ -43,11 +39,17 @@ const Document = () => {
       setIsTyping(true);
       socket.emit("document", content);
     }
-    console.log(delta);
     setValue(content);
     setTimeout(() => {
       setIsTyping(false);
     }, 1000);
+  };
+  const handleSave = () => {
+    try {
+      console.log(docId);
+    } catch (error) {
+      console.log(error);
+    }
   };
   const username = user?.username ? user?.username : "Username";
   return (
@@ -84,6 +86,12 @@ const Document = () => {
           </div>
         </div>
         <div className="w-[25%] flex items-center ">
+          <button
+            className="bg-blue-500 text-white rounded-md p-2"
+            onClick={handleSave}
+          >
+            Save
+          </button>
           <button className="bg-blue-500 text-white rounded-md p-2">
             Add Collab
           </button>

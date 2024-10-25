ad4996507dd9c0c4445bc397d3352405c6903b3f
nischalshakya <107358076+nischalshakya787@users.noreply.github.com>
2024-10-25 11:26:44 +0545

    fetching data using UserContext

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,15 +1,18 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { Home, Auth, Document } from "./pages";
+import { UserContextProvider } from "./UserContext";
 
 function App() {
   return (
     <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Auth />} />
-        <Route path="/signup" element={<Auth />} />
-        <Route path="/document/:id" element={<Document />} />
-      </Routes>
+      <UserContextProvider>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Auth />} />
+          <Route path="/signup" element={<Auth />} />
+          <Route path="/document/:id" element={<Document />} />
+        </Routes>
+      </UserContextProvider>
     </Router>
   );
 }

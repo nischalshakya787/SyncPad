d727b5b8d989bf8be88d6656b7789396281fd36b
nischalshakya <107358076+nischalshakya787@users.noreply.github.com>
2024-10-29 19:56:05 +0545

    login messsage

--- client/src/components/RecentDocumentsSection.tsx
+++ client/src/components/RecentDocumentsSection.tsx
@@ -24,13 +24,12 @@ const RecentDocumentsSection: React.FC<RecentDocsProps> = ({ user }) => {
           );
 
           const data = await response.json();
-          console.log(data);
           setDocuments(data.document || []); // Ensure data is an array
         } catch (error) {
           console.error("Error fetching documents:", error);
         }
       } else {
-        setDocuments([]); //If not logged
+        setDocuments([]); //If not logged in
       }
     };
 
@@ -40,24 +39,30 @@ const RecentDocumentsSection: React.FC<RecentDocsProps> = ({ user }) => {
   return (
     <section className="flex flex-col items-center px-20 pt-10 pb-20 mt-14 w-full bg-gray-100 max-md:px-5 max-md:mt-10 max-md:max-w-full">
       <div className="flex flex-col w-full max-w-[1226px] max-md:max-w-full">
-        <h2 className="self-start text-xl font-medium text-gray-800">
-          Recent Documents
-        </h2>
-        <div className="flex flex-wrap gap-10 items-center mt-6 max-md:max-w-full">
-          {documents.length > 0 ? (
-            documents.map((doc, index) => (
-              <Link to={`/document/${doc._id}`} key={doc._id}>
-                <DocumentCard
-                  key={index}
-                  title={doc.title}
-                  date={doc.createdAt.split("T")[0]}
-                />
-              </Link>
-            ))
-          ) : (
-            <p>No recent documents found.</p>
-          )}
-        </div>
+        {user ? (
+          <>
+            <h2 className="self-start text-xl font-medium text-gray-800">
+              Recent Documents
+            </h2>
+            <div className="flex flex-wrap gap-10 items-center mt-6 max-md:max-w-full">
+              {documents.length > 0 ? (
+                documents.map((doc, index) => (
+                  <Link to={`/document/${doc._id}`} key={doc._id}>
+                    <DocumentCard
+                      key={index}
+                      title={doc.title}
+                      date={doc.createdAt.split("T")[0]}
+                    />
+                  </Link>
+                ))
+              ) : (
+                <p>No recent documents found.</p>
+              )}
+            </div>
+          </>
+        ) : (
+          <p>Please log in to view Recent Documents</p>
+        )}
       </div>
     </section>
   );
